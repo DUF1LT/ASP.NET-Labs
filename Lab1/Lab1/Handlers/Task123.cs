d08c34967329d30930c0146a4b33deae07685405
Vadim Narkevich <Vadzim.Narkevich@itechart-group.com>
2022-09-04 17:19:06 +0300

    Add lab1

---
+++ Lab1/Lab1/Handlers/Task123.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace Lab1.Handlers
+{
+    public class Task123 : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var parameterA = context.Request.Params["ParmA"];
+            var parameterB = context.Request.Params["ParmB"];
+
+            context.Response.Write($"{context.Request.HttpMethod}-Http-NVV: ParmA = {parameterA}, ParmB = {parameterB}");
+        }
+
+        public bool IsReusable => true;
+    }
+}
