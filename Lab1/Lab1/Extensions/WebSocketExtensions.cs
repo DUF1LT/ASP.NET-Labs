c4c878baa4ff0a54bdf8ebd995a0fc556aee4a5a
Vadim Narkevich <Vadzim.Narkevich@itechart-group.com>
2022-09-04 20:35:48 +0300

    Add lab1b (web socket)

---
+++ Lab1/Lab1/Extensions/WebSocketExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Threading.Tasks;
+
+namespace Lab1.Extensions
+{
+    public static class WebSocketExtensions
+    {
+        public static async Task<string> Receive(this WebSocket socket)
+        {
+            var buffer = new ArraySegment<byte>(new byte[512]);
+            var result = await socket.ReceiveAsync(buffer, System.Threading.CancellationToken.None);
+            return System.Text.Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+        }
+
+        public static async Task Send(this WebSocket socket, string message)
+        {
+            var buffer = new ArraySegment<byte>(System.Text.Encoding.UTF8.GetBytes(message));
+            await socket.SendAsync(buffer, WebSocketMessageType.Text, true, System.Threading.CancellationToken.None);
+        }
+    }
+}
---
+++ Lab1/Lab1/Handlers/WebSocketHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.WebSockets;
+using Lab1.Extensions;
+
+namespace Lab1.Handlers
+{
+    public class WebSocketHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            if (context.IsWebSocketRequest)
+            {
+                context.AcceptWebSocketRequest(HandleWebSocketRequest);
+
+                return;
+            }
+
+            var fileName = AppDomain.CurrentDomain.BaseDirectory + "Views\\ws.html";
+            context.Response.WriteFile(fileName);
+        }
+
+        private static async Task HandleWebSocketRequest(AspNetWebSocketContext context)
+        {
+            var socket = context.WebSocket;
+
+            var initialMessage = await socket.Receive();
+            await socket.Send(initialMessage);
+
+            while (socket.State == WebSocketState.Open)
+            {
+                var currentTime = DateTime.Now.ToString(CultureInfo.CurrentCulture);
+
+                await Task.Delay(2000);
+                await socket.Send(currentTime);
+            }
+        }
+
+        public bool IsReusable => true;
+    }
+}
